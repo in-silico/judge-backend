296854c8153c20fd70e7c3738aec73124ae1cec8
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-01-17 14:08:24 -0500

    Exporting app.js as express middleware

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-var http = require('http'),
-    os   = require('os'),
-    express = require('express'),
+var express = require('express'),
     logger  = require('morgan'),
     colors  = require('colors'),
     mongoose = require('mongoose'),
@@ -25,25 +23,4 @@ require('./routes/users')(app, '/users');
 require('./routes/problems')(app, '/problems');
 require('./routes/submissions')(app, '/submissions');
 
-
-var server = http.createServer(app);
-var port = process.env.PORT || 8080;
-
-server.listen(port, function(err) {
-  if (err) throw err;
-  console.log('Starting server\nAvailable on:'.yellow);
-  var ifaces = os.networkInterfaces();
-  Object.keys(ifaces).forEach(function (dev) {
-    ifaces[dev].forEach(function (details) {
-      if (details.family === 'IPv4') {
-        console.log(('  ' + 'http://' + details.address + ':' + port.toString()).green);
-      }
-    });
-  });
-});
-
-process.on('SIGINT', function() {
-  console.log("\nShutdown the server, bye (:".yellow);
-  db.close();
-  process.exit();
-});
+module.exports = app;
