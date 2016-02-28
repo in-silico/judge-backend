e5f425f384ca3f75e47447db466a1b9e2cba01e2
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-02-28 17:07:05 -0500

    Update oauth.js

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ var ids = {
   github: {
     clientID: 'get_your_own',
     clientSecret: 'get_your_own',
-    callbackURL: "http://127.0.0.1:8080/auth/github/callback"
+    callbackURL: "http://api.judge.is/auth/github/callback"
   }
 };
 
