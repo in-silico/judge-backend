2f8004c23b46bd6cf6a3381b915435411ab9f051
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-02-28 17:02:44 -0500

    changed url, now taken from config

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var getUser = require('../local/get_user');
 
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config/' + env)
+
 
 module.exports = function(app, mountPoint, passport) {
 
@@ -9,7 +12,7 @@ module.exports = function(app, mountPoint, passport) {
       passport.authenticate('github', { failureRedirect: '/login' }),
       function(req, res) {
         // Successful authentication, redirect home.
-        res.redirect('http://judge.is');
+        res.redirect(config.frontend.url);
   });
 
   router.get('/', function(req, res) {
